422b5b18796811b7b905196df1e29232e768ce15
m-n-taylor <michael.n.taylor.biz@gmail.com>
2018-11-17 12:00:00 -0500

    Improve order-items-details-api-request-data.ts

--- old-angular-app/src/app/shared/models/order-items-details-api-request-data.ts
+++ old-angular-app/src/app/shared/models/order-items-details-api-request-data.ts
@@ -7,3 +7,4 @@ import { UserAPIRequestData } from './user-api-request-data';
 export class OrderItemsDetailsAPIRequestData extends UserAPIRequestData {
     public a: number; // Order ID
 }
+// update: 2025-07-31T20:23:00.335075
