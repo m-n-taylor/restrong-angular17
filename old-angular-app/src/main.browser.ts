bfc9ea3b1026c94bec17014caa3d3021bb71b6c3
m-n-taylor <michael.n.taylor.biz@gmail.com>
2023-05-02 12:40:00 -0400

    Fix typo in main.browser.ts

--- old-angular-app/src/main.browser.ts
+++ old-angular-app/src/main.browser.ts
@@ -6,3 +6,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserAppModule } from './app/browser-app.module';
 
 platformBrowserDynamic().bootstrapModule(BrowserAppModule);
+
+// update: 2025-08-01T01:05:23.793592
