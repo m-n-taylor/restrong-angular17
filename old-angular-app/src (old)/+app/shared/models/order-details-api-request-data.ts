028413ee38ee95a7b86ccb0033c00c2286db3b05
m-n-taylor <michael.n.taylor.biz@gmail.com>
2023-08-17 12:20:00 -0400

    Minor tweaks in order-details-api-request-data.ts

--- old-angular-app/src (old)/+app/shared/models/order-details-api-request-data.ts
+++ old-angular-app/src (old)/+app/shared/models/order-details-api-request-data.ts
@@ -12,3 +12,5 @@ export class OrderDetailsAPIRequestData extends UserAPIRequestData {
 // update: 2025-07-31T20:27:13.377715
 
 // update: 2025-08-01T01:08:39.148087
+
+// update: 2025-08-01T01:10:23.046669
